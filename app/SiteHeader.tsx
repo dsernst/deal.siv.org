fdc35c59f0d0e8caa3df7fb11a6e2928bb8ac7ef
Ariana I. <git@arianaivan.com>
2026-07-07 15:47:02 -0700

    SiteHeader appears everywhere

--- app/SiteHeader.tsx
+++ app/SiteHeader.tsx
@@ -1,13 +1,6 @@
-'use client'
-
-import { usePathname } from 'next/navigation'
-
 import { description, title } from './constants'
 
 export function SiteHeader() {
-  const pathname = usePathname()
-  if (pathname?.startsWith('/b/')) return null
-
   return (
     <>
       <h1 className="text-4xl font-bold mb-1">{title}</h1>
